2a682338627ae550a0e0407c3559ba2e46da2419
JoePaxton <paxtonjf@email.appstate.edu>
2015-02-25 18:15:36 -0500

    Update researchmodule.py

--- researchmodule.py
+++ researchmodule.py
@@ -2,10 +2,9 @@ __author__ = 'JoePaxton'
 import echonest.remix.audio as audio
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt2
 
 """
-Prints a  plot of the mean pitches over the segments during the track.
+Prints a plot of the mean pitches and timbre over the segments during the track.
 You can change the Track_ID on line 12 to a song you prefer.
 Author: Joe Paxton
 """
@@ -20,28 +19,54 @@ def main():
         collect_t.append(seg.timbre)
         
     x=0
-    y=0
-    y_t = 0
+    y=0    
         
     for i in range(len(collect)): 
         x = i
         y = np.mean(collect[i])
-        plt.bar(x, y)
-        
-    for i in range(len(collect_t)): 
-        x = i
         y_t = np.mean(collect_t[i])
-        plt2.scatter(x, y_t)
+        print '\rIterating through segments...',i,
+        plt.scatter(x, y)
+
+    plt.title('Scatter Plot of Mean Values for Pitches by Segments')
+    plt.xlabel('Segments')
+    plt.ylabel('Pitches')  
+    plt.show()      
         
+    for i in range(len(collect)): 
+        x = i        
+        y = np.mean(collect_t[i])
+        print '\rIterating through segments...',i,
+        plt.scatter(x, y)
+
     plt.title('Scatter Plot of Mean Values for Pitches by Segments')
     plt.xlabel('Segments')
     plt.ylabel('Pitches')  
     plt.show()
-    
-    plt2.title('Bar Plot of Mean Values for Pitches by Segments')
-    plt2.xlabel('Segments')
-    plt2.ylabel('Pitches')
-    pl2.show()
+
+    for i in range(len(collect)): 
+        x = i
+        y = np.mean(collect[i])
+        y_t = np.mean(collect_t[i])
+        print '\rIterating through segments...',i,
+        plt.bar(x, y)
+
+    plt.title('Bar Plot of Mean Values for Timbre by Segments')
+    plt.xlabel('Segments')
+    plt.ylabel('Timbre')  
+    plt.show()      
+        
+    for i in range(len(collect)): 
+        x = i        
+        y = np.mean(collect_t[i])
+        print '\rIterating through segments...',i,
+        plt.bar(x, y)
+
+    plt.title('Bar Plot of Mean Values for Timbre by Segments')
+    plt.xlabel('Segments')
+    plt.ylabel('Timbre')  
+    plt.show()
+        
     return
 
 if __name__ == '__main__':
